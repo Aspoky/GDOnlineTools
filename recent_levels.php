d18375e0209c04b1450bb94dc02daec9715f01ea
Aspoky <32813748+Aspoky@users.noreply.github.com>
2018-06-03 01:35:57 +0200

    Add files via upload

---
+++ recent_levels.php
@@ -0,0 +1,8 @@
+<head>
+<title>Get recent levels</title>
+<style>h1{margin-bottom:0.5rem;}hr{margin-bottom:1.5rem;}iframe{width:100%;height:100%;border:0;}</style>
+</head>
+<h1>Get recent levels</h1>
+<hr>
+<h4>Click submit</h4>
+<iframe src='do.php'></iframe>
---
+++ src/do_rc.php
@@ -0,0 +1,28 @@
+<style>.main{display:none;}</style>
+<form method='POST' action='http://www.boomlings.com/database/getGJLevels21.php' id='form'>
+<div class='main'>
+Type:<input type='text' name='type' id='type' value='4'>
+<br/><br/>
+str:<input type='text' name='str' id='str'>
+<br/><br/>
+diff:<input type='text' name='diff' id='diff' value='-'>
+<br/><br/>
+len:<input type='text' name='len' id='len' value='-'>
+<br/><br/>
+page:<input type='text' name='page' id='page' value='0'>
+<br/><br/>
+total:<input type='text' name='total' id='total' value='1'>
+<br/><br/>
+secret:<input type='text' name='secret' id='secret' value='Wmfd2893gb7'>
+<br/><br/></div>
+<input type='submit' name='submit' id='submit' value='Submit'><br/>
+</form>
+<br/>
+<?php
+echo "<b>Posting to:</b> http://www.boomlings.com/database/getGJLevels21.php";
+echo "<br/>";
+echo "<br/>";
+echo "<i>This tool is created by Aspoky</i>";
+?>
+<br/>
+<br/>
